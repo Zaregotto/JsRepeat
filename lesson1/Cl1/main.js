f26bfb5240b1a0d568bfbf1f9820cc0942a31e59
Zaregotto <Zaregotto307@gmail.com>
2022-02-04 17:20:13 +0200

    Cls1

---
+++ lesson1/Cl1/main.js
@@ -0,0 +1,63 @@
+// 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+
+// let ten = [1,2,3,4,5,6,7,8,9,10];
+// let result = 0;
+// function sum() {
+//     for (i = 0; i < ten.length; i++) {
+//         result += ten[i];
+//     }
+//     return result
+// }
+//
+// console.log(sum());
+
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+
+// let book = {
+//     name: 'kk',
+//     number: '45',
+//     genre: 'phantasy'
+// }
+// console.log(book)
+
+
+// . Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+
+// let book = {
+//     name: 'kk',
+//     number: '45',
+//     genre: 'phantasy',
+//     author: 'I'
+// }
+// console.log(book)
+
+
+// 4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+
+// let book = [{name: 'kk1', number: 50, genre: 'phantasy', author: 'I',}, {name: 'aa1', number: 60, genre: 'phantasy', author: 'You'}];
+// console.log(book);
+
+// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// Значення площі зберігати в змінній s.
+
+// let height = 23
+// let width = 10
+// let s = height*width
+// console.log(s)
+
+// Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
+
+// let heightC = 10;
+// let dC = 4;
+// let p = 3.14;
+// let v = Math.pow(dC,2)*p*heightC
+// console.log(v);
+
+// У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
+
+// let n = 3;
+// let m = 4;
+// let k = Math.pow(n,2) + Math.pow(m,2);
+// console.log(Math.sqrt(k));
