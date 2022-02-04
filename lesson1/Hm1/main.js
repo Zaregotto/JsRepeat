d7958c52580c0e42910219f625a023ef7865e890
Zaregotto <Zaregotto307@gmail.com>
2022-02-04 17:19:53 +0200

    hm1

---
+++ lesson1/Hm1/main.js
@@ -0,0 +1,180 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let hello = 'hello';
+// console.log(hello);
+// alert(hello);
+// document.write(hello);
+// let owu = 'owu';
+// console.log(owu);
+// alert(owu);
+// document.write(owu);
+// let com = 'com';
+// console.log(com);
+// alert(com);
+// document.write(com);
+// let ua = 'ua';
+// console.log(ua);
+// alert(ua);
+// document.write(ua);
+// let number1 = 1;
+// console.log(number1);
+// alert(number1);
+// document.write(number1);
+// let number2 = 10;
+// console.log(number2);
+// alert(number2);
+// document.write(number2);
+// let number3 = -999;
+// console.log(number3);
+// alert(number3);
+// document.write(number3);
+// let number4 = 123;
+// console.log(number4);
+// alert(number4);
+// document.write(number4);
+// let pi = 3.14;
+// console.log(pi);
+// alert(pi);
+// document.write(pi);
+// let e = 2.7;
+// console.log(e);
+// alert(e);
+// document.write(e);
+// let number5 = 16;
+// console.log(number5);
+// alert(number5);
+// document.write(number5);
+// let yes = true;
+// console.log(yes);
+// alert(yes);
+// document.write(yes);
+// let no = false;
+// console.log(no);
+// alert(no);
+// document.write(no);
+
+
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let hello = 'hi';
+// console.log(hello);
+// alert(hello);
+// document.write(hello);
+// let owu = 'octen';
+// console.log(owu);
+// alert(owu);
+// document.write(owu);
+// let com = 'net';
+// console.log(com);
+// alert(com);
+// document.write(com);
+// let ua = 'ru';
+// console.log(ua);
+// alert(ua);
+// document.write(ua);
+// let number1 = 2;
+// console.log(number1);
+// alert(number1);
+// document.write(number1);
+// let number2 = 20;
+// console.log(number2);
+// alert(number2);
+// document.write(number2);
+// let number3 = -7777;
+// console.log(number3);
+// alert(number3);
+// document.write(number3);
+// let number4 = 321;
+// console.log(number4);
+// alert(number4);
+// document.write(number4);
+// let pi = 3.1415926;
+// console.log(pi);
+// alert(pi);
+// document.write(pi);
+// let e = 2.71828;
+// console.log(e);
+// alert(e);
+// document.write(e);
+// let number5 = 36;
+// console.log(number5);
+// alert(number5);
+// document.write(number5);
+// let yes = false;
+// console.log(yes);
+// alert(yes);
+// document.write(yes);
+// let no = true;
+// console.log(no);
+// alert(no);
+// document.write(no);
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+
+// let firstName = 'Дичко'
+// let middleName = 'Микола'
+// let lastName = 'Андрійович'
+//
+// let person = `${firstName} ${middleName} ${lastName}`;
+// console.log(person)
+
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+// let name = prompt('your name');
+// let middlename = prompt('middle name');
+// let age = prompt('age');
+//
+// console.log(`Вітаю ${name} ${middlename}. Тобі ${age} років`);
+
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+// let a = 100;
+// console.log(typeof a);
+//
+// let b = '100';
+// console.log(typeof b);
+//
+// let c = true;
+// console.log(typeof c);
+
+
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+// 5 ? 6 -> true
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 10 ? 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 ? '123' -> false
+// 123 ? '123' -> true
+
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 === 6);
+// console.log(5 >= 6);
+// console.log(10 === 10);
+// console.log(10 >= 10);
+// console.log(10 !== 10);
+// console.log(10 > 10);
+// console.log(10 < 10);
+// console.log(123 > '123');
+// console.log(123 == '123');
+
+
+// - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>"); // 205
+// document.write(str - a + "<br/>"); // 15
+// document.write(str * "2" + "<br/>"); // 40
+// document.write(str / 2 + "<br/>"); // 10
